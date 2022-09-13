612bb98179a712b121ad2574d98f86bf59914b29
Franco <82281@sistemas.frc.utn.edu.ar>
2022-09-13 19:36:46 -0300

    Se cambio el nombre de archivo y completo con Entrada y Salida

---
+++ Clase_1/Entrada_Salida_deDatos.py
@@ -0,0 +1,28 @@
+# Clase 1
+
+# "#" Sirve para escribir comentarios, la forma correcto de usarlo es un espacio y empezar con letra mayuscula
+
+# Salida de Datos
+
+# PRINT - Mostrar
+# Con comillas dobles o  simples
+print ('Que onda loco')
+print ("Que onda loco?")
+# Pero si quiero mostrar comillas dentro de mi print
+print("Que onda 'loco'?")
+# Con \n hacemos un salto de linea y con \t una tabulacion
+print ("Que onda loco?\n")
+print ("Que onda \t loco?")
+# Podemos concatenar dentro delPrint con +
+print ("Que onda loco?" +" "+ "Todo bien pa")
+# O podemos dejar un espacio entre variables con SEP
+print ("Que onda loco?", "Todo bien pa", sep= " ")
+# SEP sirve para concatenar cualquier variable entre variables
+print ("Ri","er es de la ", "",sep= "b")
+
+#Entrada de Datos
+
+# Con INPUT podemos leer cadenas que nos ingresa el usuario
+input("Porfavor ingrese un numero: ")
+
+
