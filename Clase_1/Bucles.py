74ac08913d51cfe5a38e2fb1a3decf90eee88737
Franco <82281@sistemas.frc.utn.edu.ar>
2022-09-24 20:26:42 -0300

    Se agrego clase con bucles

---
+++ Clase_1/Bucles.py
@@ -0,0 +1,16 @@
+# FOR
+
+lista = ["Leandro", "Milton" , "Barreto", "Marcone"]
+for i in lista:
+    print(i)
+
+exit
+
+listajugadores = []
+input("Ingrese un jugador: ")
+
+for i in range(10):
+    listajugadores.append(input("Ingrese otro jugador: "))
+
+print(listajugadores)
+# WHILE
