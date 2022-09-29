1d0c1caccb39d77a3acacb83daa2d8a9104f533d
Franco <82281@sistemas.frc.utn.edu.ar>
2022-09-29 00:25:54 -0300

    Se agrego carpeta Utilidades con su init para conservar paquetes

--- Clases PIL/Utilidades/Paquetes.py
+++ Clases PIL/Utilidades/Paquetes.py
@@ -5,3 +5,12 @@
 
 # Con el comando PIP LIST en el CMD podemos ver la lista de paquetes instalados en el entorno virtual
 # ahora con esta lista, podemos hacer PIP FREEZE para hace runa "captura de pantalla" y con > la metemos en un txt llamado "requirements.txt"
+
+# Tambien puedo instalar requirements.txt de otros proyectos. Para eso creo un nuevo entorno virtual con "python venv env", luego meto en la carpeta
+# del entorno el requirements.txt del proyecto nuevo y lo instalo con "PIP INSTALL -R y el \txt"
+
+import Funciones
+
+Funciones.prueba2(a, b, c)
+
+# Vamos a crear ua carpeta "UTILIDADES" para guardar nuestros propios paquetes, y con el archivo "_init_.py" python lo va a identificar
